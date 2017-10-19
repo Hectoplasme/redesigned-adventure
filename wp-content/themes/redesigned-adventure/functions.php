dc6aa35d656e569558b9400328af8d034d360934
Laurianne <laurianne@spintank.fr>
2017-10-19 20:52:39 +0200

    Add custom field plugin

--- wp-content/themes/redesigned-adventure/functions.php
+++ wp-content/themes/redesigned-adventure/functions.php
@@ -1,9 +1,42 @@
 <?php
+// 1. customize ACF path
+add_filter('acf/settings/path', 'my_acf_settings_path');
+
+function my_acf_settings_path( $path ) {
+
+    // update path
+    $path = get_template_directory() . '/inc/acf/';
+
+    // return
+    return $path;
+}
+
+// 2. customize ACF dir
+add_filter('acf/settings/dir', 'my_acf_settings_dir');
+
+function my_acf_settings_dir( $dir ) {
+
+    // update path
+    $dir = get_template_directory_uri() . '/inc/acf/';
+
+    // return
+    return $dir;
+
+}
+// 3. Hide ACF field group menu item
+add_filter('acf/settings/show_admin', '__return_false');
+
+
+// 4. Include ACF
+include_once( get_template_directory() . '/inc/acf/acf.php' );
+include_once( get_template_directory() . '/inc/acf-export.php' );
+
 /**
 * make the theme customizer ready
 **/
 
 include( get_template_directory() . '/inc/customizer.php' );
+
 
 /**
 * pouet_register_menus - register menus
