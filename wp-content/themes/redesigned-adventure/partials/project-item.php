13dcb9f234e138c5522894a4d888ccf2c9ea4155
Laurianne <laurianne@spintank.fr>
2017-10-21 21:14:45 +0200

    Add categories to project item

--- wp-content/themes/redesigned-adventure/partials/project-item.php
+++ wp-content/themes/redesigned-adventure/partials/project-item.php
@@ -1,6 +1,15 @@
 <div class="container project__inner">
     <h2 class="project__title heading-2"><?php the_title(); ?></h2>
-    <p class="project__subtitle teasing-2">Identité graphique & développement</p>
+    <p class="project__subtitle teasing-2"><?php $categories = get_categories();
+    for ($i = 0; $i < count($categories); $i ++):
+        if ($i < count($categories) - 2):
+            echo $categories[$i]->name . ', ';
+        elseif ($i === count($categories) - 2):
+            echo $categories[$i]->name . ' & ';
+        else :
+            echo $categories[$i]->name;
+        endif;
+    endfor; ?></p>
     <article class="project__content rte">
      <?php the_content(); ?>
     </article>
