d57857a3d653efc92c4067a188a61ca01020deaa
Laurianne <laurianne@spintank.fr>
2017-10-29 13:21:12 +0100

    keep only the theme

--- index.php
+++ index.php
@@ -1,17 +1,80 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<?php get_header(); ?>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+<main class="main" role="main">
+  <section class="banner" id="accueil">
+    <div class="banner__bg u-b-pattern"></div>
+    <div class="container banner__inner">
+      <div class="banner__title rte">
+        <h1><?php the_field('baseline', false, false);?></h1>
+      </div>
+    </div>
+  </section>
+  <?php
+  $projects = get_posts(
+      array(
+          'posts_per_page'    => 4,
+          'orderby' => 'menu_order',
+          'order' => 'ASC'
+      )
+  );
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+  if( $projects ):
+      foreach ($projects as $i => $project):
+          $post = $project;
+          setup_postdata($post);
+          if ($i === 0): ?>
+            <section class="section project section--dark" id="projets">
+          <?php elseif ($i%2 === 0) : ?>
+            <section class="section project section--dark">
+          <?php  else :?>
+            <section class="section project">
+          <?php endif;?>
+          <?php get_template_part('partials/project-item'); ?>
+            </section>
+          <?php wp_reset_postdata();
+      endforeach;
+  endif; ?>
+  <!-- <section class="section project section--dark" id="projets">
+    <div class="container project__inner">
+      <h2 class="project__title heading-2">Luditales</h2>
+      <p class="project__subtitle teasing-2">Identité graphique & développement</p>
+      <article class="project__content rte">
+        <p>Cat ipsum dolor sit amet, hack up furballs, sit on human and chase dog then run away, or run in circles. Lick arm hair chirp at birds. Scratch the postman wake up lick paw wake up owner meow meow spot something, big eyes, big eyes, crouch, shake butt, prepare to pounce and hide from vacuum cleaner intrigued by the shower.</p>
+        <p><strong>Inspect anything brought</strong> into the house find empty spot in cupboard and <a href="#">sleep all day</a> so sit on the laptop, intently sniff hand thinking longingly about tuna brine spend all night ensuring people don't sleep sleep all day.</p>
+      </article>
+      <button class="btn">Voir le projet</button>
+    </div>
+  </section>
+  <section class="section project">
+    <div class="container project__inner">
+      <h2 class="project__title heading-2">Luditales</h2>
+      <p class="project__subtitle teasing-2">Identité graphique & développement</p>
+      <article class="project__content rte">
+        <p>Cat ipsum dolor sit amet, hack up furballs, sit on human and chase dog then run away, or run in circles. Lick arm hair chirp at birds. Scratch the postman wake up lick paw wake up owner meow meow spot something, big eyes, big eyes, crouch, shake butt, prepare to pounce and hide from vacuum cleaner intrigued by the shower.</p>
+        <p><strong>Inspect anything brought</strong> into the house find empty spot in cupboard and <a href="#">sleep all day</a> so sit on the laptop, intently sniff hand thinking longingly about tuna brine spend all night ensuring people don't sleep sleep all day.</p>
+      </article>
+      <a href="" rel="external" class="btn">Voir le projet</a>
+    </div>
+  </section> -->
+  <section class="section u-b-grey" id="a-propos">
+    <div class="container">
+      <h2 class="section__title heading-1"><?php the_field('about_title', false, false);?></h2>
+      <div class="row">
+        <div class="section__left column is-one-third-desktop">
+            <?php $text = get_field('about_text');?>
+            <div class="section__teasing teasing-1 rte">
+                <?php echo $text['about_heading']; ?>
+            </div>
+        </div>
+        <div class="section__right column is-two-third">
+          <article class="section__content rte">
+              <?php echo $text['about_content']; ?>
+          </article>
+          <a href="<?php echo get_field('about_cv')['url']; ?>" target="_blank" class="btn">Voir mon CV</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+
+<?php get_footer(); ?>
