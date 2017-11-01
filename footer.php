aed1011c724bb5db8afaac05397f9c6659474aed
Laurianne <laurianne@spintank.fr>
2017-11-01 17:13:52 +0100

    Fix for error pages

--- footer.php
+++ footer.php
@@ -1,22 +1,21 @@
 <footer class="footer u-b-black section section--dark" id="contact">
   <div class="container footer__inner">
-    <h2 class="section__title heading-1"><?php the_field('contact_title', false, false);?></h2>
+    <h2 class="section__title heading-1"><?php the_field('contact_title', get_option( 'page_on_front' ), false);?></h2>
     <div class="row">
       <div class="column is-one-third-desktop">
-        <p class="section__teasing teasing-1"><?php the_field('contact_text', false, false);?></p>
+        <p class="section__teasing teasing-1"><?php the_field('contact_text', get_option( 'page_on_front' ), false);?></p>
       </div>
     </div>
     <nav class="footer__nav">
       <ul class="menu">
-          <?php $links = get_field('contact_links');
+          <?php $links = get_field('contact_links', get_option( 'page_on_front' ));
           if ($links) :
               foreach($links as $link_item): ?>
                         <li><a href="<?php echo $link_item['link']['url']; ?>" target="_blank"><?php echo $link_item['link']['title']; ?></a></li>
           <?php endforeach;
             endif;?>
       </ul>
-      <p class="footer__bottom u-c-grey-dark copyright">©2017 Laurianne Terrier, All Rights Reserved | <a href="#">Mentions légales</a></p>
-    </nav>
+      <p class="footer__bottom u-c-grey-dark copyright"><?php the_field('contact_copyright', get_option( 'page_on_front' ), false); ?>
   </div>
 </footer>
 
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <div class="banner__bg u-b-pattern"></div>
     <div class="container banner__inner">
       <div class="banner__title rte">
-        <h1><?php the_field('baseline', false, false);?></h1>
+        <h1><?php the_field('baseline', get_option( 'page_on_front' ), false);?></h1>
       </div>
     </div>
   </section>
@@ -34,34 +34,12 @@
           <?php wp_reset_postdata();
       endforeach;
   endif; ?>
-  <!-- <section class="section project section--dark" id="projets">
-    <div class="container project__inner">
-      <h2 class="project__title heading-2">Luditales</h2>
-      <p class="project__subtitle teasing-2">Identité graphique & développement</p>
-      <article class="project__content rte">
-        <p>Cat ipsum dolor sit amet, hack up furballs, sit on human and chase dog then run away, or run in circles. Lick arm hair chirp at birds. Scratch the postman wake up lick paw wake up owner meow meow spot something, big eyes, big eyes, crouch, shake butt, prepare to pounce and hide from vacuum cleaner intrigued by the shower.</p>
-        <p><strong>Inspect anything brought</strong> into the house find empty spot in cupboard and <a href="#">sleep all day</a> so sit on the laptop, intently sniff hand thinking longingly about tuna brine spend all night ensuring people don't sleep sleep all day.</p>
-      </article>
-      <button class="btn">Voir le projet</button>
-    </div>
-  </section>
-  <section class="section project">
-    <div class="container project__inner">
-      <h2 class="project__title heading-2">Luditales</h2>
-      <p class="project__subtitle teasing-2">Identité graphique & développement</p>
-      <article class="project__content rte">
-        <p>Cat ipsum dolor sit amet, hack up furballs, sit on human and chase dog then run away, or run in circles. Lick arm hair chirp at birds. Scratch the postman wake up lick paw wake up owner meow meow spot something, big eyes, big eyes, crouch, shake butt, prepare to pounce and hide from vacuum cleaner intrigued by the shower.</p>
-        <p><strong>Inspect anything brought</strong> into the house find empty spot in cupboard and <a href="#">sleep all day</a> so sit on the laptop, intently sniff hand thinking longingly about tuna brine spend all night ensuring people don't sleep sleep all day.</p>
-      </article>
-      <a href="" rel="external" class="btn">Voir le projet</a>
-    </div>
-  </section> -->
   <section class="section u-b-grey" id="a-propos">
     <div class="container">
-      <h2 class="section__title heading-1"><?php the_field('about_title', false, false);?></h2>
+      <h2 class="section__title heading-1"><?php the_field('about_title', get_option( 'page_on_front' ), false);?></h2>
       <div class="row">
         <div class="section__left column is-one-third-desktop">
-            <?php $text = get_field('about_text');?>
+            <?php $text = get_field('about_text', get_option( 'page_on_front' ));?>
             <div class="section__teasing teasing-1 rte">
                 <?php echo $text['about_heading']; ?>
             </div>
