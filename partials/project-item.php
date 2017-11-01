785440b7df7dabc4810e19394d7349e9f8a1fd8d
Laurianne <laurianne@spintank.fr>
2017-11-01 17:26:06 +0100

    Fix category list

--- partials/project-item.php
+++ partials/project-item.php
@@ -1,6 +1,6 @@
 <div class="container project__inner">
     <h2 class="project__title heading-2"><?php the_title(); ?></h2>
-    <p class="project__subtitle teasing-2"><?php $categories = get_categories();
+    <p class="project__subtitle teasing-2"><?php $categories = get_the_category();
     for ($i = 0; $i < count($categories); $i ++):
         if ($i < count($categories) - 2):
             echo $categories[$i]->name . ', ';
